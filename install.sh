60b65b51a3a624ef16c5b6a2c76bea993b6347b0
Yeffly <60810753+Yeffly@users.noreply.github.com>
2023-12-18 13:17:46 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -433,7 +433,7 @@ ssl_install() {
     fi
     judge "安装 SSL 证书生成脚本依赖"
 
-    curl https://get.acme.sh | sh
+    curl https://get.acme.sh | sh -s email=my@example.com
     judge "安装 SSL 证书生成脚本"
 }
 
